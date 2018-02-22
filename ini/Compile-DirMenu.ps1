8224b6bbc1ef6f72ff500c203a0494ec4bd30302
turboBasic <off@boun.cr>
2018-02-22 15:01:30 +0200

    Encoding fixed

--- ini/Compile-DirMenu.ps1
+++ ini/Compile-DirMenu.ps1
@@ -1,32 +1,32 @@
 Set-Variable -Name n -Value 0
 
-Get-Content -Path .\DirMenu.ini.src -Encoding utf8 |
+Get-Content -Path .\dirMenu.ini.src -Encoding utf8 |
 ForEach-Object -Process {
   if ($_ -match "^menu=") {
     $n++;
     $_ -replace "^menu=", "menu$n="
   }
-  elseif ($_ -match '^cmd=') {
-    $_ -replace "^cmd=", "cmd$n="
+  elseif ($_ -match '^\s*cmd=') {
+    $_ -replace "\s*cmd=", "cmd$n="
   }
   else { $_ }
 } |
-Set-Content -Path DirMenu-new.ini -Encoding Unicode
+Set-Content -Path dirMenu-new.ini -Encoding Unicode
 Remove-Variable  n
 
 
 Try {
-    Rename-Item -Path DirMenu.ini -NewName DirMenu.ini.bak -Force -ErrorAction Stop
+    Rename-Item -Path dirMenu.ini -NewName dirMenu.ini.bak -Force -ErrorAction Stop
 }
 Catch [System.Management.Automation.ItemNotFoundException] {
   "item not found"
 }
 Catch {
   "any other undefined errors"
-  Remove-Item -Path DirMenu.ini.bak -Force
-  Rename-Item -Path DirMenu.ini -NewName DirMenu.ini.bak -Force
+  Remove-Item -Path dirMenu.ini.bak -Force
+  Rename-Item -Path dirMenu.ini -NewName dirMenu.ini.bak -Force
   #$error[0]
 }
 Finally {
-  Rename-Item -Path DirMenu-new.ini -NewName DirMenu.ini
+  Rename-Item -Path dirMenu-new.ini -NewName dirMenu.ini
 }
